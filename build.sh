c13716a7bb1b8b90ae6a1836d0a1211e2c67b28a
dannymg302-cyber <danny.mg302@gmail.com>
2026-08-31 21:34:13 -0500

    1.1 Beta

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 lb clean
-lb config --distribution trixie --architectures amd64 --binary-images iso-hybrid --bootloaders "grub-efi syslinux" --archive-areas "main contrib non-free non-free-firmware" --iso-application "KebianOS" --iso-volume "KebianOS 1.0 Beta" --iso-publisher "KebianOS Team" --iso-preparer "KebianOS"
+lb config --distribution trixie --architectures amd64 --binary-images iso-hybrid --bootloaders "grub-efi syslinux" --archive-areas "main contrib non-free non-free-firmware" --iso-application "KebianOS" --iso-volume "KebianOS 1.1 Beta" --iso-publisher "KebianOS Team" --iso-preparer "KebianOS"
 lb build
